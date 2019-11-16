5d9faa9996054f8a3ee7f2608c30138066e07378
Mustafa Yalciner <mustafayalciner@outlook.de>
2019-11-16 03:54:50 +0100

    Define Main class and include documentation in the dependency.

---
+++ src/main/java/runnable/Main.java
@@ -0,0 +1,18 @@
+package runnable;
+import moa.streams.generators.RandomRBFGenerator;
+/**
+ *
+ * @author Mustafa Yalciner
+ */
+public class Main {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        RandomRBFGenerator randomGenerator = new RandomRBFGenerator();
+        // TODO code application logic here
+    }
+    
+}
