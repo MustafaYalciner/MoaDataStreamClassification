ba5ace2e993f82d7aacfd543064bdee2d148dda8
Mustafa Yalciner <mustafayalciner@outlook.de>
2019-11-17 15:01:51 +0100

    Implement the creation of the data set and save it in the data file. The dataset is provided aswell.

--- src/main/java/runnable/Main.java
+++ src/main/java/runnable/Main.java
@@ -1,18 +1,49 @@
 package runnable;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import moa.core.InstanceExample;
 import moa.streams.generators.RandomRBFGenerator;
+
 /**
  *
  * @author Mustafa Yalciner
  */
 public class Main {
-    
+
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        final int dataSize = 10000;
         System.out.println("Hello world");
-        RandomRBFGenerator randomGenerator = new RandomRBFGenerator();
+        RandomRBFGenerator randomGen = new RandomRBFGenerator();
+        randomGen.prepareForUse();
+
+        File fout = new File("data.txt");
+        FileOutputStream fos = new FileOutputStream(fout);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+
+        for (int i = 0; i < dataSize; i++) {
+            InstanceExample example = randomGen.nextInstance();
+            double[] row = example.getData().toDoubleArray();
+            for (int j = 0; j < row.length; j++) {
+                bw.write(String.valueOf(row[j]));
+                if (j < row.length - 1) {
+                    bw.write(",");
+                }
+            }
+            if (i < dataSize - 1) {
+                bw.newLine();
+            }
+        }
+        bw.close();
+
         // TODO code application logic here
     }
-    
+
 }
