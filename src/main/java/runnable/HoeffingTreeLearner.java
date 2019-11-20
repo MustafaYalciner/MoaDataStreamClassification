7391a3244ec29eec16be9a65c7b44b2ee7118cab
Mustafa Yalciner <mustafayalciner@outlook.de>
2019-11-20 15:08:47 +0100

    Implement the majority vote classifier based on the combination of hoefftrees and naivebayes classifier.

--- src/main/java/runnable/HoeffingTreeLearner.java
+++ src/main/java/runnable/HoeffingTreeLearner.java
@@ -2,11 +2,13 @@ package runnable;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import moa.classifiers.Classifier;
 import moa.classifiers.bayes.NaiveBayes;
 import moa.classifiers.trees.HoeffdingTree;
 import moa.core.InstanceExample;
 import moa.core.SerializeUtils;
+import moa.core.Utils;
 import moa.streams.generators.RandomRBFGenerator;
 
 /**
@@ -20,9 +22,10 @@ public class HoeffingTreeLearner {
      */
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         final int dataSize = 10000;
-
-        
-        RandomRBFGenerator randomGen = (RandomRBFGenerator)SerializeUtils.readFromFile(new File("SerializedDataSet.txt"));
+        boolean isNaiveCorrect;
+        boolean isHoeffCorrect;
+        boolean isMajorityVoteCorrect;
+        RandomRBFGenerator randomGen = (RandomRBFGenerator) SerializeUtils.readFromFile(new File("SerializedDataSet.txt"));
         randomGen.prepareForUse();
 
         Classifier hoeffTreeClassifier = new HoeffdingTree();
@@ -35,17 +38,42 @@ public class HoeffingTreeLearner {
         naiveBayesClassifier.prepareForUse();
         double naiveCorrectCounter = 0;
 
+        double majorityVoteCorrectCounter = 0;
         for (int i = 0; i < dataSize; i++) {
             InstanceExample example = randomGen.nextInstance();
-            boolean isHoeffCorrect = hoeffTreeClassifier.correctlyClassifies(example.getData());
+            isHoeffCorrect = hoeffTreeClassifier.correctlyClassifies(example.getData());
             hoeffCorrectCounter = isHoeffCorrect ? hoeffCorrectCounter + 1 : hoeffCorrectCounter;
+            System.out.println("isHoeffCorrect? " + isHoeffCorrect);
+            System.out.println("Prediction: "
+                    + Arrays.toString(hoeffTreeClassifier.getPredictionForInstance(example.getData()).getVotes()));
+
             hoeffTreeClassifier.trainOnInstance(example.getData());
-            boolean isNaiveCorrect = naiveBayesClassifier.correctlyClassifies(example.getData());
+            isNaiveCorrect = naiveBayesClassifier.correctlyClassifies(example.getData());
             naiveCorrectCounter = isNaiveCorrect ? naiveCorrectCounter + 1 : naiveCorrectCounter;
+
+            System.out.println("naiveBayesClassifier? " + isNaiveCorrect);
+            System.out.println("Prediction: "
+                    + Arrays.toString(naiveBayesClassifier.getPredictionForInstance(example.getData()).getVotes()));
             naiveBayesClassifier.trainOnInstance(example.getData());
+            isMajorityVoteCorrect = majorityVoteClassifier(naiveBayesClassifier.getPredictionForInstance(example.getData()).getVotes(),
+                    hoeffTreeClassifier.getPredictionForInstance(example.getData()).getVotes())
+                    == (int) example.getData().classValue();
+            majorityVoteCorrectCounter = isMajorityVoteCorrect
+                    ? majorityVoteCorrectCounter + 1
+                    : majorityVoteCorrectCounter;
         }
         System.out.println("Data size: " + dataSize);
         System.out.println("HOEFF Accuracy: " + hoeffCorrectCounter / dataSize);
         System.out.println("Naive Accuracy: " + naiveCorrectCounter / dataSize);
+        System.out.println("Majority vote Accuracy: " + majorityVoteCorrectCounter / dataSize);
     }
+
+    private static int majorityVoteClassifier(double[] arr1, double[] arr2) {
+        double[] retArr = new double[arr1.length];
+        for (int i = 0; i < retArr.length; i++) {
+            retArr[i] = arr1[i] + arr2[i];
+        }
+        return Utils.maxIndex(retArr);
+    }
+
 }
